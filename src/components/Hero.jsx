feb9d5672f96a1916a519e702b4d6fa3027b0323
Emerson Davenheimer <ejdavenheimer@gmail.com>
2025-01-31 22:32:39 -0300

    Header listo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,74 +1,95 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 function Hero() {
-    return (
-        <section className="bg-transparent pt-32 pb-20 px-4">
-            <div className="container mx-auto flex flex-col md:flex-row items-center">
-                <div className="w-full md:w-1/2 mb-10 md:mb-0">
-                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary-blue mb-4 font-titles">
-                        Desarrollador Full Stack
-                    </h1>
-                    <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-primary-green mb-6 font-body">
-                        Emerson Davenheimer
-                    </h2>
-                    <div className="flex space-x-4">
-                        <a
-                            href="#" // Reemplaza # con la URL de contacto
-                            className="bg-primary-green hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg inline-flex items-center"
-                        >
-                            Contáctame
-                        </a>
-                        <a
-                            href="#" // Reemplaza # con la URL de tu GitHub
-                            className="bg-gray-700 hover:bg-gray-800 text-white font-medium py-2 px-4 rounded-lg inline-flex items-center"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="mr-2"
-                            >
-                                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-                            </svg>
-                            GitHub
-                        </a>
-                        <a
-                            href="#" // Reemplaza # con la URL de tu LinkedIn
-                            className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg inline-flex items-center"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="mr-2"
-                            >
-                                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-                                <path d="M22 6l-10 7-10-7" />
-                                <path d="M2 17h20" />
-                            </svg>
-                            LinkedIn
-                        </a>
-                    </div>
-                </div>
-                <div className="w-full md:w-1/2">
-                    {/* Espacio para la imagen */}
-                    <div className="bg-gray-300 h-64 md:h-96"></div>
-                </div>
+  const emailRef = useRef(null);
+  const [isCopied, setIsCopied] = useState(false);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [navbarHeight, setNavbarHeight] = useState(0);
+
+  useEffect(() => {
+    const navHeight = document.querySelector('nav').offsetHeight;
+    setNavbarHeight(navHeight);
+
+    const handleResize = () => {
+      const navbarHeight = document.querySelector('nav').offsetHeight;
+      setNavbarHeight(navbarHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const copyEmailToClipboard = () => {
+    navigator.clipboard.writeText(emailRef.current.innerText)
+      .then(() => {
+        setIsCopied(true);
+        setShowTooltip(true);
+        setTimeout(() => {
+          setIsCopied(false);
+          setShowTooltip(false);
+        }, 2000);
+      })
+      .catch(err => {
+        console.error('Error al copiar: ', err);
+      });
+  };
+
+  return (
+    <>
+      <nav className="fixed top-0 bg-white/10 backdrop-blur-md text-white w-full z-10">
+        {/* ... (Contenido del Navbar, no cambia) */}
+      </nav>
+
+      <section className="bg-transparent px-4 md:px-10 lg:px-20" style={{ paddingTop: navbarHeight + "px" }}>
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-center" style={{ minHeight: `calc(100vh - ${navbarHeight}px)` }}>
+          <div className="w-full md:w-1/2 mb-10 md:mb-0">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary-blue mb-4 font-titles">
+              Desarrollador Full Stack
+            </h1>
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-primary-green mb-6 font-body">
+              Emerson Davenheimer
+            </h2>
+            <div className="flex items-stretch border w-80 border-gray-800 rounded-l-md bg-gray-700 relative">
+              <label
+                htmlFor="email"
+                className="text-gray-300 font-bold px-2 py-1 whitespace-nowrap rounded-l border-r border-gray-800 bg-gray-600 flex items-center"
+              >
+                Contáctame
+              </label>
+              <span
+                ref={emailRef}
+                className="text-gray-50 px-2 py-1 select-all flex-grow"
+                id="email"
+              >
+                ejdavenheimer@gmail.com
+              </span>
+              <button
+                onClick={copyEmailToClipboard}
+                className="py-1 px-2 rounded-r-md bg-gray-500 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-primary-blue relative"
+                title={isCopied ? "Copiado!" : "Copiar correo"}
+              >
+                {!isCopied && <i className="fa-regular fa-copy text-gray-300" />}
+                {isCopied && <i className="fas fa-check text-gray-800" />}
+                {showTooltip && (
+                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 bg-gray-800 text-white px-2 py-1 rounded-md text-sm pointer-events-none whitespace-nowrap">
+                    ¡Copiado!
+                  </div>
+                )}
+              </button>
             </div>
-        </section>
-    );
+          </div>
+          <div className="w-full md:w-1/4 absolute bottom-0 right-0 md:static flex justify-end">
+            <img
+              src="./src/assets/images/me.png"
+              alt="Emerson Davenheimer"
+              className="w-full md:w-auto rounded-lg shadow-lg object-contain"
+              style={{ maxHeight: `calc(100vh - ${navbarHeight}px - 100px)` }}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Hero;
